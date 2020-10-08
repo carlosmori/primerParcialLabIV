ca6a98316157f11e82cb001f411fbd20737b1d30
Carlos Mori <cmori.94@gmail.com>
2020-10-08 18:50:26 -0300

    initial config, prettier, eslint

---
+++ primerParcial/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBVt4Do6yRoOp0gnRt3eB1ea1JH2RP7kTM",
+  authDomain: "moricarlosprimerparcial.firebaseapp.com",
+  databaseURL: "https://moricarlosprimerparcial.firebaseio.com",
+  projectId: "moricarlosprimerparcial",
+  storageBucket: "moricarlosprimerparcial.appspot.com",
+  messagingSenderId: "505756818642",
+  appId: "1:505756818642:web:63d6b79c2217e386b4e10b",
+  measurementId: "G-3V41B2E86D"
+}
+};
---
+++ primerParcial/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBVt4Do6yRoOp0gnRt3eB1ea1JH2RP7kTM",
+  authDomain: "moricarlosprimerparcial.firebaseapp.com",
+  databaseURL: "https://moricarlosprimerparcial.firebaseio.com",
+  projectId: "moricarlosprimerparcial",
+  storageBucket: "moricarlosprimerparcial.appspot.com",
+  messagingSenderId: "505756818642",
+  appId: "1:505756818642:web:63d6b79c2217e386b4e10b",
+  measurementId: "G-3V41B2E86D"
+}
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
